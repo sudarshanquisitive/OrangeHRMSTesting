600f948e00ab462798fe873904317a0bf492c219
K Sudarshan Rao <sudarshan.kondam@quisitive.com>
2024-11-15 14:51:41 +0530

    Chrome browser issue

--- src/test/java/com/OrangeHRMS_Test/LoginTest.java
+++ src/test/java/com/OrangeHRMS_Test/LoginTest.java
@@ -2,6 +2,7 @@ package com.OrangeHRMS_Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,51 +24,28 @@ public class LoginTest {
 	@BeforeTest
 	public void Before() {
 
-		//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
-		//		driver = new ChromeDriver();
-
-		ChromeOptions options = new ChromeOptions();
-		// Set the binary location based on the environment
-		//        String ENV_NAME = System.getenv("ENV_NAME");
-		//        if ("production".equals(ENV_NAME)) {
-		//            options.setBinary("/opt/render/.wdm/drivers/chromedriver");
-		//        }
-
-		// Add the necessary arguments
-		options.setBinary("/opt/render/.wdm/drivers/chromedriver");
-		options.addArguments("--verbose");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--headless");
-		options.addArguments("--ignore-certificate-errors");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--remote-debugging-port=9222");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--disable-gpu");
-
-
-		System.out.println("Chrome browser");
-
 		WebDriverManager.chromedriver().setup();
-		//WebDriverManager.chromedriver().driverVersion("130.0.6723.117").setup();
-		driver = new ChromeDriver();
-
-		System.out.println("The browser value is Chrome");
-		driver.get(URL);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+		driver.navigate().to(URL);
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	@Test
 	public void Login() throws InterruptedException {
 		driver.findElement(By.name("username")).click();
 		driver.findElement(By.name("username")).clear();
 		driver.findElement(By.name("username")).sendKeys("Admin");
-
+		
 		driver.findElement(By.name("password")).click();
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		Thread.sleep(5000);
 		//driver.findElement(By.xpath("@type,'submit'")).click();
-
+		System.out.println("***********Login TC Pass******************");
 	}
 	@AfterTest
 	public void quit() {
