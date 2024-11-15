55dffea7ccd1bfb3b89bdb6972940097ab74cfb8
K Sudarshan Rao <sudarshan.kondam@quisitive.com>
2024-11-15 10:32:02 +0530

    Update LoginTest.java
    Chromedriver updates

--- src/test/java/com/OrangeHRMS_Test/LoginTest.java
+++ src/test/java/com/OrangeHRMS_Test/LoginTest.java
@@ -19,61 +19,59 @@ public class LoginTest {
 
 	WebDriver driver;
 	String URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
-	
+
 	@BeforeTest
 	public void Before() {
-	
-//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
-//		driver = new ChromeDriver();
-		
-		ChromeOptions options = new ChromeOptions();
-		
-		        
-		//WebDriverManager.chromedriver().setup();
-		WebDriverManager.chromedriver().driverVersion("130.0.6723.117").setup();
-		driver = new ChromeDriver();
-		
-		 // Set the binary location based on the environment
-//        String ENV_NAME = System.getenv("ENV_NAME");
-//        if ("production".equals(ENV_NAME)) {
-//            options.setBinary("/opt/render/.wdm/drivers/chromedriver");
-//        }
 
-        // Add the necessary arguments
-        options.setBinary("/opt/render/.wdm/drivers/chromedriver");
-        options.addArguments("--verbose");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-debugging-port=9222");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-gpu");
+		//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+		//		driver = new ChromeDriver();
+
+		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.chromedriver().driverVersion("130.0.6723.117").setup();
+		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		// Set the binary location based on the environment
+		//        String ENV_NAME = System.getenv("ENV_NAME");
+		//        if ("production".equals(ENV_NAME)) {
+		//            options.setBinary("/opt/render/.wdm/drivers/chromedriver");
+		//        }
+
+		// Add the necessary arguments
+		options.setBinary("/opt/render/.wdm/drivers/chromedriver");
+		options.addArguments("--verbose");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--headless");
+		options.addArguments("--ignore-certificate-errors");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--remote-debugging-port=9222");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-gpu");
+
 
 		System.out.println("Chrome browser");
-		
+
 		System.out.println("The browser value is Chrome");
 		driver.get(URL);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 		driver.manage().window().maximize();
-		}
+	}
 	@Test
 	public void Login() throws InterruptedException {
 		driver.findElement(By.name("username")).click();
 		driver.findElement(By.name("username")).clear();
 		driver.findElement(By.name("username")).sendKeys("Admin");
-		
+
 		driver.findElement(By.name("password")).click();
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		Thread.sleep(5000);
 		//driver.findElement(By.xpath("@type,'submit'")).click();
-		
+
 	}
 	@AfterTest
 	public void quit() {
 		driver.quit();
 	}
-	
+
 
 }
